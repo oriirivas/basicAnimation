bdef0a63d2833e8ca1f5716de2ec2a5f1e179891
oriana.rivas <oriana.rivas@zentagroup.com>
2021-01-26 19:25:59 -0300

    animaciones anidadas

--- src/app/basic-animation/basic/animation/animation.component.ts
+++ src/app/basic-animation/basic/animation/animation.component.ts
@@ -10,6 +10,27 @@ export class AnimationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.changeAnimation()
+    
+  }
+  
+  public changeAnimation() {
+    const pelota = document.getElementById('pelotaTres');
+    pelota?.addEventListener('animationend', (event) => {
+      console.log(event);
+      if(event.animationName === 'rebote'){
+        console.log('entre en rebote');
+        pelota.style.animationName='pelota, escalera, color'
+        pelota.style.animationDuration='3s'
+        pelota.style.animationIterationCount='infinite, 2, infinite'
+      }
+      if(event.animationName === 'escalera'){
+        console.log('entre en escalera');
+        pelota.style.animationName='pelota, rebote, color'
+        pelota.style.animationDuration='3s, .3s, 1s'
+        pelota.style.animationIterationCount='infinite, 20'
+      }
+    })
   }
 
 }
